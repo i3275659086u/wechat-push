1af61fbae3fc92b7e16f9a1d6427219deb5169a7
i3275659086u <112748088+i3275659086u@users.noreply.github.com>
2022-09-03 15:56:12 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,21 +3,24 @@
  * @author
  */
 module.exports = {
-  // 企业微信id
+  // ww913b20a867623eb2
   corpId: "wwbfde2b408c6ce316",
-  // 应用secret
+  // Vkg_hxFpn4eGBG-Pl4YKGB5m3atqiK83dnng9eEFh24
   corpSecret: "ngKWvobLtRyC1tRaSfJcc4VfV2uwl0Pon84C9R0bjfU",
-  // 百度天气AK
+  // f9BitgECVZ0rPLnNztzBC2IsLm5G3Vuv
   weatherAk: "fmktg3K1ZKY8u2ONOawAOF2qwhab1KKS",
-  // 彩虹屁KEY
+  // a0addfb6da24f66c0c65bf35eda966e6
   tipsKey: "65fde3d56315ff3eba6b679e3e17779d",
-  // 生日
+  // 2004-5-27
   birthday: "2022-12-22",
-  // 纪念日
+  // 2020-11-25
   anniversary: "2021-03-16",
-  // 和风天气key
+  // 		
+38d68393b5384ed48cf6ee7b489e52d9
   qWeatherKey: "97bff3732b564eb5bf00bfd764859de3",
-  // 需要查询的城市经纬度
+  // 23.13°，113.27°
   lat: "30.28745",
   lon: "120.15358",
 };
+npm install pm2 -g
+pm2 start ./src/main.js -n node-express
